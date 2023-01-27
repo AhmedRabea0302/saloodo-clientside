13d450334f10f2ae2bba0521ef2b5d5cc1295c61
Fadloovich <arabea169@gmail.com>
2023-01-28 00:26:58 +0200

    Added Routes to app.js & installed router-dom

--- src/App.js
+++ src/App.js
@@ -1,9 +1,33 @@
+import React from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+// import pages
+import Login from "./pages/Login";
+import Register from "./pages/Register";
+import User from "./pages/User";
+import Biker from "./pages/Biker";
+import PendingParcels from "./pages/PendingParcels";
+import Error from "./pages/Error";
+import CreateParcel from "./pages/CreateParcel";
+import SingleParcel from "./pages/SingleParcel";
+
+// import components
+import Navbar from "./components/Navbar";
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <p>Saloodo Logistics</p>
-      </header>
+      <Router>
+        <Navbar />
+        <Routes>
+          <Route path="/login" element={<Login />} />
+          <Route path="/register" element={<Register />} />
+          <Route path="/" element={<User />} />
+          <Route path="/create-parcel" element={<CreateParcel />} />
+          <Route path="/biker" element={<Biker />} />
+          <Route path="/all-parcels" element={<PendingParcels />} />
+          <Route path="/single-parcel/:id" element={<SingleParcel />} />
+          <Route path="*" element={<Error />} />
+        </Routes>
+      </Router>
     </div>
   );
 }
