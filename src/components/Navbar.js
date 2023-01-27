473ec50046615c80987b4e143c0c57c1dbd86d83
Fadloovich <arabea169@gmail.com>
2023-01-28 01:08:21 +0200

    Add navbar links and styles

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,5 +1,28 @@
+import logo from "../logo.svg";
+import { Link } from "react-router-dom";
+
 const Navbar = () => {
-  return <div>Navbar</div>;
+  return (
+    <nav className="navbar">
+      <div className="nav-center">
+        <Link to="/">
+          <img className="logo" alt="Saloodo" src={logo}></img>
+        </Link>
+        <ul className="nav-links">
+          <li>
+            <Link to="/">My Parcels</Link>
+          </li>
+          <li>
+            <Link to="/create-parcel">Create Parcel</Link>
+          </li>
+
+          <li>
+            <button className="btn btn-primary">Log Out</button>
+          </li>
+        </ul>
+      </div>
+    </nav>
+  );
 };
 
 export default Navbar;
