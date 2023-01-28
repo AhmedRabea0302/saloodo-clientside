d05d4123a1fa3f35d6407249bb594e17aed38364
Fadloovich <arabea169@gmail.com>
2023-01-28 06:58:25 +0200

    Added Biker parcels list to pick from

--- src/pages/PendingParcels.js
+++ src/pages/PendingParcels.js
@@ -1,5 +1,14 @@
+import ParcelsList from "../components/ParecelsList";
+
 const PendingParcels = () => {
-  return <div>Pending Parcels</div>;
+  return (
+    <section className="section">
+      <h2 className="section-title">
+        All Pending Parcels, Start Hunting Some!
+      </h2>
+      <ParcelsList parcelstype="bikerparcels" />
+    </section>
+  );
 };
 
 export default PendingParcels;
